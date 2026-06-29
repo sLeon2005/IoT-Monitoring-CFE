8d4648b19da4e4277e38c28dd93b0e9fb9230bf9
Sebastián León M. <sleonmedellin@gmail.com>
2026-06-29 13:14:14 -0600

    main con métodos sin json

--- cfe_api/main.py
+++ cfe_api/main.py
@@ -1,7 +1,15 @@
 from core.session import CFESession
+from services.concursos import ConcursosService
 
 session = CFESession()
-
 session.initialize()
 
-print(session.token)
+concursos = ConcursosService(session)
+
+datos = concursos.buscar_por_fecha("2026-06-29")
+
+print(f"Se encontraron {len(datos)} concursos")
+
+for concurso in datos:
+    print(concurso.numero, end=" - ")
+    print(concurso.descripcion)
